a243906593a14e2e9e5efc940900a561733585aa
cookfront <cookfront@gmail.com>
2016-05-14 23:29:48 +0800

    use felint-config and add help message

--- index.js
+++ index.js
@@ -3,15 +3,30 @@
 var program = require('commander');
 var colors = require('colors');
 var childProcess = require('child_process');
+var process = require('process');
+var fs = require('fs');
 
 program
-  .version('0.1.0')
   .command('init')
+  .description('init git hook, you can configure you git hook url by .felintrc file')
   .action(function() {
     console.log('init git hooks:\n'.green);
+    var currWorkDirectory = process.cwd();
+    var felintrcFileObj;
+    try {
+      felintrcFileObj = JSON.parse(fs.readFileSync(currWorkDirectory + '/.felintrc').toString());
+    } catch (e) {
+      felintrcFileObj = {};
+    }
+    var gitHookUrl;
+    if (typeof felintrcFileObj === 'object' && felintrcFileObj.gitHookUrl) {
+      gitHookUrl = felintrcFileObj.gitHookUrl;
+    } else {
+      gitHookUrl = 'https://github.com/youzan/felint-config.git';
+    }
     var processPromise = new Promise(function(resolve, reject) {
       childProcess.exec(
-        'git clone https://github.com/cookfront/git-hooks.git git_hooks',
+        'git clone ' + gitHookUrl + ' .git_hooks',
         function(err) {
           if (err) {
             reject();
@@ -24,7 +39,7 @@ program
     processPromise
     .then(function(res) {
       childProcess.exec(
-        'sh ./git_hooks/update_git_hooks.sh',
+        'sh ./.git_hooks/update_git_hooks.sh',
         function(err) {
           if (err) {
             console.log(colors.red('Error: please try again'));
@@ -41,11 +56,12 @@ program
 
 program
   .command('update')
+  .description('update git hooks')
   .action(function() {
     console.log('update git hooks:\n'.green)
     var processPromise = new Promise(function(resolve, reject) {
       childProcess.exec(
-        'cd ./git_hooks && git pull origin master',
+        'cd ./.git_hooks && git pull origin master',
         function(err) {
           if (err) {
             reject();
@@ -58,13 +74,13 @@ program
     processPromise
     .then(function() {
       childProcess.exec(
-        'sh ./git_hooks/update_git_hooks.sh',
+        'sh ./.git_hooks/update_git_hooks.sh',
         function(err) {
           if (err) {
             console.log(colors.red('Error: please try again'));
           }
             
-          console.log(colors.green('init git hooks success!'));
+          console.log(colors.green('update git hooks success!'));
         }
       );
     })
@@ -74,3 +90,7 @@ program
   });
 
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
