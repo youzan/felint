f43290b7f7d3e72362829a75a0f3a2b0067814eb
lianchengjie <ctyuna@gmail.com>
2016-06-03 22:07:56 +0800

    add options

--- index.js
+++ index.js
@@ -6,95 +6,128 @@ var childProcess = require('child_process');
 var process = require('process');
 var fs = require('fs');
 
+// update config files
+function updateConfig() {
+    console.log('update git hooks:\n'.green)
+    var processPromise = new Promise(function(resolve, reject) {
+        childProcess.exec(
+            'cd ./.git_hooks && git pull origin dev',
+            function(err) {
+                if (err) {
+                    console.log(err, '\n');
+                    reject();
+                } else {
+                    resolve();
+                }
+            }
+        );
+    });
+    return processPromise;
+}
+
+// init config files
+function initConfig() {
+    console.log('start init config files...\n'.green);
+    var currWorkDirectory = process.cwd();
+    var felintrcFileObj = {};
+    var felintrcPwd = currWorkDirectory + '/.felintrc';
+
+    var resolveFn;
+    var rejectFn;
+    var processPromise = new Promise(function(resolve, reject) {
+        resolveFn = resolve;
+        rejectFn = reject;
+    });
+
+    fs.access(felintrcPwd, fs.F_OK | fs.R_OK, function(err) {
+        if (err) {
+            console.log('.felintrc file does not exist or can not be read, please check it\n'.red)
+        } else {
+            try {
+                felintrcFileObj = JSON.parse(fs.readFileSync(felintrcPwd).toString());
+            } catch (e) {
+                console.log('parse .felintrc file error\n');
+            }
+        }
+        var gitHookUrl;
+        if (typeof felintrcFileObj === 'object' && felintrcFileObj.gitHookUrl) {
+            gitHookUrl = felintrcFileObj.gitHookUrl;
+            console.log('use .felintrc file\n'.green)
+        } else {
+            gitHookUrl = 'https://github.com/youzan/felint-config.git';
+            console.log('use default config...\n'.green)
+        }
+        console.log(colors.green('use ' + gitHookUrl) + '\n( you can use your own, via https://github.com/youzan/felint/blob/master/README.md )\n');
+        console.log('getting the config files from remote server...\n'.green);
+        childProcess.exec(
+            'rm -rf ./.git_hooks && git clone -b dev ' + gitHookUrl + ' .git_hooks',
+            function(err) {
+                if (err) {
+                    console.log(err, '\n');
+                    rejectFn();
+                }
+                resolveFn();
+            }
+        );
+    });
+    return processPromise;
+}
+
+
+// run logic shell
+function runSh() {
+    var esV = program.ecamScript6 ? '6' : '5';
+    console.log('start run logic shell...\n'.green)
+    var child = childProcess.exec(
+        'sh ./.git_hooks/update_git_hooks.sh ' + esV,
+        function(err) {
+            if (err) {
+                console.log(err);
+                console.log('\n');
+                console.log(colors.red('Error: please try again'));
+            } else {
+                console.log(colors.green('update git hooks success! ') + '\n( eslint will be run while git commit )\n');
+                console.log(colors.green('update .eslintrc .scss-lint.yml success!\n'));
+                console.log(colors.green('Enjoy!'));
+            }
+        }
+    );
+    child.stdout.on('data', function(data) {
+        console.log(data);
+    })
+}
+
 program
-    .version('0.1.5')
+    .option('-5, --ecamScript5', 'default ecamScript5 for your project')
+    .option('-6, --ecamScript6', 'default ecamScript6 for your project');
+
+program
+    .version('0.1.6')
     .command('init')
     .description('by default, felint will copy the eslint config file, css lint config and git hooks \
       from https://github.com/youzan/felint-config. \
       You can use your own by forking our felint-config and specifying the git url in .felintrc file. \
       More detail please read: https://github.com/youzan/felint/blob/master/README.md')
     .action(function() {
-        console.log('detecting felint-config git repository url...\n'.green);
-        var currWorkDirectory = process.cwd();
-        var felintrcFileObj;
-        try {
-            felintrcFileObj = JSON.parse(fs.readFileSync(currWorkDirectory + '/.felintrc').toString());
-        } catch (e) {
-            felintrcFileObj = {};
-        }
-        var gitHookUrl;
-        if (typeof felintrcFileObj === 'object' && felintrcFileObj.gitHookUrl) {
-            gitHookUrl = felintrcFileObj.gitHookUrl;
-        } else {
-            gitHookUrl = 'https://github.com/youzan/felint-config.git';
-        }
-        console.log(colors.green('use ' + gitHookUrl) + '\n( you can use your own, via https://github.com/youzan/felint/blob/master/README.md )\n');
-        console.log('getting the config files from remote server...\n'.green);
-        var processPromise = new Promise(function(resolve, reject) {
-            childProcess.exec(
-                'rm -rf ./.git_hooks && git clone ' + gitHookUrl + ' .git_hooks',
-                function(err) {
-                    if (err) {
-                        reject();
-                    }
-                    resolve();
-                }
-            );
+
+        initConfig().then(function(res) {
+            runSh();
+        }).catch(function() {
+            console.log(colors.red('Error: please try again'));
         });
 
-        processPromise
-            .then(function(res) {
-                childProcess.exec(
-                    'sh ./.git_hooks/update_git_hooks.sh',
-                    function(err) {
-                        if (err) {
-                            console.log(colors.red('Error: please try again'));
-                        }
-
-                        console.log(colors.green('init git hooks success! ') + '\n( eslint will be run while git commit )\n');
-                        console.log(colors.green('init .eslintrc .scss-lint.yml success!\n'));
-                        console.log(colors.green('Enjoy!'));
-                    }
-                );
-            })
-            .catch(function() {
-                console.log(colors.red('Error: please try again'));
-            });
     });
 
 program
     .command('update')
     .description('update git hooks')
     .action(function() {
-        console.log('update git hooks:\n'.green)
-        var processPromise = new Promise(function(resolve, reject) {
-            childProcess.exec(
-                'cd ./.git_hooks && git pull origin master',
-                function(err) {
-                    if (err) {
-                        reject();
-                    }
-                    resolve();
-                }
-            );
+        updateConfig().then(function() {
+            runSh();
+        }).catch(function() {
+            console.log(colors.red('Error: please try again'));
         });
 
-        processPromise
-            .then(function() {
-                childProcess.exec(
-                    'sh ./.git_hooks/update_git_hooks.sh',
-                    function(err) {
-                        if (err) {
-                            console.log(colors.red('Error: please try again'));
-                        }
-
-                        console.log(colors.green('update git hooks success!'));
-                    }
-                );
-            })
-            .catch(function() {
-                console.log(colors.red('Error: please try again'));
-            });
     });
 
 program.parse(process.argv);
